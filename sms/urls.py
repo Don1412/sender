48815d492d8e147c047ddf7f6db84339d3fd2af8
Ghoul <densmorgon@gmail.com>
2018-04-08 20:03:59 +0300

    Removed useless files from the repo. Fixed login and register forms. New index template and view.

--- sms/urls.py
+++ sms/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('sms/', views.RegisterView)
+    path('', views.IndexView.as_view(), name='index'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
