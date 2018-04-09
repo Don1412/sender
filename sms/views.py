7cde55c01cb1987f48c5c2ce9384721d00680d54
Ghoul <densmorgon@gmail.com>
2018-04-10 02:46:59 +0300

    Changed templates getting logic.

--- sms/views.py
+++ sms/views.py
@@ -1,5 +1,6 @@
 from django import views
 from django.contrib.auth import logout, authenticate, login
+from django.forms import model_to_dict
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import NameTemplate, MessageTemplate
@@ -76,8 +77,8 @@ class CreateView(views.View):
     def get(self, request):
         name_template_items = NameTemplate.objects.filter(user=self.request.user)
         message_template_items = MessageTemplate.objects.filter(user=self.request.user)
-        return render(request, 'create.html', {'name_template_items':name_template_items,
-                                               'message_template_items':message_template_items})
+        return render(request, 'create.html', {'name_template_items': name_template_items,
+                                               'message_template_items': message_template_items})
 
     def post(self, request):
         return 1
@@ -85,11 +86,9 @@ class CreateView(views.View):
 
 class NameTemplateView(views.View):
     def get(self, request):
-        try:
-            data = NameTemplate.objects.filter(name=request.GET.get('name'), user=self.request.user)
-            return HttpResponse('OK')
-        except NameTemplate.DoesNotExist:
-            return HttpResponse('NotExist')
+        templates = NameTemplate.objects.filter(name=request.GET.get('name'), user=self.request.user)
+        data = {'templates': [model_to_dict(t) for t in templates]}
+        return JsonResponse(data)
 
     def post(self, request):
         print(request)
@@ -97,12 +96,11 @@ class NameTemplateView(views.View):
 
 class MessageTemplateView(views.View):
     def get(self, request):
-        try:
-            data = MessageTemplate.objects.get(name=request.GET.get('name'), user=self.request.user)
-            result = {'name': data.name, 'text': data.text, 'type': data.type}
-            return JsonResponse(result)
-        except MessageTemplate.DoesNotExist:
-            return HttpResponse(MessageTemplate.DoesNotExist, content_type='application/json')
+        templates = MessageTemplate.objects.filter(name=request.GET.get('name'), user=self.request.user)
+        result = {
+            'templates': [model_to_dict(template) for template in templates]
+        }
+        return JsonResponse(result)
 
     def post(self, request):
         return 1
